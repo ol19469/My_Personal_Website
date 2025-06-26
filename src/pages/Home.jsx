e6112d0f60da1479abf728a22d97f1e8dcb3aa4e
ol19469 <ol19469@gmail.com>
2025-06-25 20:19:04 -0500

    mostly finished about page. still needs animations and touch up. started home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,20 @@
-import AnimatedPage from "../components/AnimatedPage";
+import { Link } from "react-router-dom";
 import "./Home.css";
+import homeBg from "../assets/images/campfire_background.png";
+import fire from "../assets/animations/fire.json";
+import Lottie from "lottie-react";
+import AnimatedPage from "../components/AnimatedPage";
 
 export default function Home() {
   return (
     <AnimatedPage>
-      <section className="home-page">
-        <h1>Welcome to My Campfire</h1>
-        <p>Warm yourself by the fire and explore the forest.</p>
-      </section>
+      <div className="home-container">
+        <img src={homeBg} alt="Forest background" className="home-bg" />
+        <Lottie animationData={fire} loop={true} className="fire-animation" />
+        <Link to="/projects" className="nav-button telescope-btn" />
+        <Link to="/about" className="nav-button cabin-btn" />
+        <Link to="/contact" className="nav-button dock-btn" />
+      </div>
     </AnimatedPage>
   );
 }
