a2b53f64999a4201ee3fb2476aeade04b0f95451
ol19469 <ol19469@gmail.com>
2025-06-27 17:35:10 -0500

    added loaded state for background images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./Home.css";
 import homeBg from "../assets/images/campfire_background.png";
 import fire from "../assets/animations/fire.json";
@@ -6,10 +7,22 @@ import Lottie from "lottie-react";
 import AnimatedPage from "../components/AnimatedPage";
 
 export default function Home() {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <AnimatedPage>
       <div className="home-container">
-        <img src={homeBg} alt="Forest background" className="home-bg" />
+        {!imageLoaded && <div className="loading-spinner"></div>}
+        <img
+          src={homeBg}
+          alt="Forest background"
+          className={`home-bg ${imageLoaded ? "loaded" : ""}`}
+          onLoad={handleImageLoad}
+        />
         <Lottie animationData={fire} loop={true} className="fire-animation" />
         <Link to="/projects" className="nav-button telescope-btn" />
         <Link to="/about" className="nav-button cabin-btn" />
